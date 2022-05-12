d0d6561bb8bf88dc194a88dad0ed1fd9a2582461
MGTEDITS <marcobertolo05@outlook.pt>
2022-05-12 14:32:42 +0100

    CEA

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
         </div>
         <div class="carousel-item">
             <img id="background1" class="d-block w-100">
-            <div class="carousel-caption text-start" style="  top: 15rem; left: 5rem">
+            <div class="carousel-caption text-start" style="top: 15rem; left: 5rem">
                 <h1>Projeto Rios</h1>
                 <p class="w-50">It is a long established fact that a reader will be distracted by the readable
                     content of a page when looking at its layout. The point of using Lorem Ipsum
